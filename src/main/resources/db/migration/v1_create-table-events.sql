1d88f78f44bd0fb17fd01dda27614deb48b391fb
Hiego <hiegobarreto@gmail.com>
2024-04-02 11:40:52 -0300

    initial api structure

---
+++ src/main/resources/db/migration/v1_create-table-events.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "events" (
+                          id VARCHAR(255) NOT NULL PRIMARY KEY,
+                          title VARCHAR(255) NOT NULL,
+                          details VARCHAR(255),
+                          slug VARCHAR(255) NOT NULL,
+                          maximum_attendees INTEGER NOT NULL
+);
